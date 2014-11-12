3c149bc963ae7d236bbf03ad777d9dfe0f42a04e
maximojr <maximojr@gmail.com>
2014-11-12 14:29:54 -0200

    teste

---
+++ desktop/src/br/com/tapananuca/gereacademia/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.tapananuca.gereacademia.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.tapananuca.gereacademia.GereAcademia;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GereAcademia(), config);
+	}
+}
---
+++ html/src/br/com/tapananuca/gereacademia/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package br.com.tapananuca.gereacademia.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import br.com.tapananuca.gereacademia.GereAcademia;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GereAcademia();
+        }
+}
